670968b44476000a2b5658827c5987ceae754d1e
Kamen Yovchev <kamen.yovchev@wize-soluions.com>
2020-10-20 17:01:44 +0300

    add auditing policy variables

--- sql-server.tf
+++ sql-server.tf
@@ -13,4 +13,11 @@ resource "azurerm_sql_server" "sql-server" {
   version = var.sql_server_version
   administrator_login = var.administrator_login
   administrator_login_password = var.administrator_login_password
+
+  extended_auditing_policy {
+    storage_endpoint                        = var.storage_endpoint
+    storage_account_access_key              = var.storage_account_access_key
+    storage_account_access_key_is_secondary = var.storage_account_access_key_is_secondary
+    retention_in_days                       = var.retention_in_days
+  }
 }
--- variables.tf
+++ variables.tf
@@ -5,7 +5,13 @@ variable location {}
 variable administrator_login {}
 variable administrator_login_password {}
 variable sql_server_version {}
-
+variable storage_endpoint {}
+variable storage_account_access_key {}
+variable storage_account_access_key_is_secondary {
+    type = bool
+    default = true
+}
+variable retention_in_days {}
 ##################################################################################
 # LOCALS
 ##################################################################################
